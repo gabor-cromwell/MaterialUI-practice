be439af5c2bd9ef6fdde379dcba6242937f96ee3
Havasi <gabor.havasi@cromwell.co.uk>
2022-11-18 16:04:33 +0000

    Main page is working with the API

--- pokemon/src/Pokedex.js
+++ pokemon/src/Pokedex.js
@@ -3,14 +3,14 @@ import {
   Card,
   CardContent,
   CardMedia,
-  CircularProgress,
   Grid,
   LinearProgress,
   Toolbar,
+  Typography,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { useState } from "react";
-import mockData from "./mockData";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const useStyles = makeStyles({
   mainPageContainer: {
@@ -18,25 +18,52 @@ const useStyles = makeStyles({
     paddingLeft: "50px",
     paddingRight: "50px",
   },
+  cardMedia: {
+    margin: "auto",
+  },
+  cardContent: {
+    textAlign: "center",
+  },
 });
 
 const Pokedex = () => {
   const classes = useStyles();
-  const [pokemonData, setPokemonData] = useState(mockData);
+  const [pokemonData, setPokemonData] = useState();
 
-  const getPokemonCard = (pokemonId) => {
-    {
-      //   console.log(pokemonData[`${pokemonId}`]);
-      const { id, name } = pokemonData[`${pokemonId}`];
-      const { species } = pokemonData[`${pokemonId}`];
-      const { url } = species;
-      console.log(id, name, url);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?limit=50`
+      );
+      const data = response.data.results;
+      const pokemonObject = data.map((pokemon, index) => ({
+        id: index + 1,
+        name: pokemon.name,
+        picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          index + 1
+        }.png`,
+      }));
+      setPokemonData(pokemonObject);
     }
+    fetchData();
+  }, []);
+
+  const getPokemonCard = (pokemon) => {
+    const { id, name, picture } = pokemon;
+
     return (
-      <Grid item xs={12} sm={6} md={4} key={pokemonId}>
+      <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
         <Card>
-          <CardMedia image={"url"}></CardMedia>
-          <CardContent>{pokemonId}</CardContent>
+          <CardMedia
+            className={classes.cardMedia}
+            image={picture}
+            style={{ width: "130px", height: "130px" }}
+          />
+          <CardContent className={classes.cardContent}>
+            <Typography>
+              {id}. {name[0].toUpperCase() + name.slice(1)}
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
     );
@@ -49,9 +76,7 @@ const Pokedex = () => {
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.mainPageContainer}>
-          {Object.keys(pokemonData).map((pokemonId) =>
-            getPokemonCard(pokemonId)
-          )}
+          {pokemonData.map((pokemon) => getPokemonCard(pokemon))}
         </Grid>
       ) : (
         <LinearProgress color="secondary" />
